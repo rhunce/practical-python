5277f81bac7c94ff527e4d4744170ff6678765e5
Raymond Anthony Hunce <hunce18c@gmail.com>
2023-11-14 15:39:59 -0500

    Complete bounce.py

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,11 @@
 # bounce.py
 #
 # Exercise 1.5
+
+height = 100 # meters
+next_bounce_ratio = 3 / 5
+bounces = 10
+
+for i in range(bounces):
+    height *= next_bounce_ratio
+    print(i + 1, round(height, 4))

ef810fefb8a71ad2f23391c7058e60652368294c
Raymond Hunce <hunce18c@gmail.com>
2023-12-06 16:28:25 -0500

    Complete Exercise 1.31: Error handling

--- Work/pcost.py
+++ Work/pcost.py
@@ -14,15 +14,34 @@ name,       shares,     price
 "MSFT",     50,         65.10
 "IBM",      100,        70.44
 
+
+name,shares,price
+"AA",100,32.20
+"IBM",50,91.10
+"CAT",150,83.44
+"MSFT",,51.23
+"GE",95,40.37
+"MSFT",50,65.10
+"IBM",,70.44
 """
 
-with open("Data/portfolio.csv", "rt") as f:
-    headers = next(f)
-    cost = 0
-    for line in f:
-        stock_info = line.split(",")
-        shares = int(stock_info[1])
-        price = float(stock_info[2])
-        cost += shares * price
 
-    print(f"Total Cost: ${cost:.2f}")
+def portfolio_cost(filename):
+    with open(filename, "rt") as f:
+        headers = next(f)
+        cost = 0
+        for line in f:
+            try:
+                stock_info = line.split(",")
+                shares = int(stock_info[1])
+                price = float(stock_info[2])
+                cost += shares * price
+            except ValueError:
+                print("Couldn't parse", line, end="")
+
+        return cost
+
+
+# cost = portfolio_cost("Data/portfolio.csv")
+cost = portfolio_cost("Data/missing.csv")
+print(f"Total Cost: ${cost:.2f}")

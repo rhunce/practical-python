92c23682a02cef264ab2d94275c3cb40a1bc8c1a
Raymond Hunce <hunce18c@gmail.com>
2023-12-06 22:48:11 -0500

    Complete Exercise 2.15: A practical enumerate() example

--- Work/pcost.py
+++ Work/pcost.py
@@ -34,13 +34,13 @@ def portfolio_cost(filename):
     with open(filename, "rt") as f:
         rows = csv.reader(f)
         headers = next(rows)
-        for row in rows:
+        for rowno, row in enumerate(rows, start=1):
             try:
                 n_shares = int(row[1])
                 price = float(row[2])
                 total_cost += n_shares * price
             except ValueError:
-                print("Couldn't parse", row)
+                print(f"Row {rowno}: Bad row: {row}")
 
         return total_cost
 
@@ -51,6 +51,6 @@ else:
     filename = "Data/portfolio.csv"
 
 
-cost = portfolio_cost(filename)
-# cost = portfolio_cost("Data/missing.csv")
+# cost = portfolio_cost(filename)
+cost = portfolio_cost("Data/missing.csv")
 print(f"Total Cost: ${cost:.2f}")

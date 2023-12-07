b1a6209c74da4d712d21ca1b891d76978bb1af74
Raymond Hunce <hunce18c@gmail.com>
2023-12-06 21:11:38 -0500

    Complete Exercise 2.6: Dictionaries as a container

--- Work/report.py
+++ Work/report.py
@@ -6,7 +6,7 @@ import csv
 
 
 def read_portfolio(filename):
-    """Opens a given portfolio file and reads it into a list of tuples."""
+    """Opens a given portfolio file and reads it into a list of dictionaries."""
     portfolio = []
 
     with open(filename, "rt") as f:
@@ -16,3 +16,18 @@ def read_portfolio(filename):
             holding = {"name": row[0], "shares": int(row[1]), "price": float(row[2])}
             portfolio.append(holding)
     return portfolio
+
+
+def read_prices(filename):
+    """Opens a given prices file and reads it into a list of dictionaries."""
+    prices = {}
+
+    with open(filename, "rt") as f:
+        rows = csv.reader(f)
+        for row in rows:
+            try:
+                prices[row[0]] = float(row[1])
+            except IndexError:
+                print("Skipping empty row...")
+
+    return prices

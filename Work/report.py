eb2fe18fee4019a0227f03a065eade9f40ddba8f
Raymond Hunce <hunce18c@gmail.com>
2023-12-09 14:46:24 -0500

    Modify read_portfolio

--- Work/report.py
+++ Work/report.py
@@ -6,16 +6,16 @@ import tableformat
 from portfolio import Portfolio
 
 
-def read_portfolio(filename):
+def read_portfolio(filename, **opts):
     """
     Read a stock portfolio file into a list of dictionaries with keys
     name, shares, and price.
     """
     with open(filename) as file:
         portdicts = fileparse.parse_csv(
-            file, select=["name", "shares", "price"], types=[str, int, float]
+            file, select=["name", "shares", "price"], types=[str, int, float], **opts
         )
-        portfolio = [stock.Stock(d["name"], d["shares"], d["price"]) for d in portdicts]
+        portfolio = [stock.Stock(**d) for d in portdicts]
         return Portfolio(portfolio)
 
 

9892b9906b165b7ef776cdd8d1fef8f9af3acbf2
Raymond Hunce <hunce18c@gmail.com>
2023-12-08 20:23:38 -0500

    Complete Exercise 4.10: An example of using getattr()

--- Work/stock.py
+++ Work/stock.py
@@ -11,6 +11,9 @@ class Stock:
         self.shares = shares
         self.price = price
 
+    def __repr__(self):
+        return f"Stock({self.name!r}, {self.shares!r}, {self.price!r})"
+
     def cost(self):
         """
         Return the cost as shares*price
--- Work/tableformat.py
+++ Work/tableformat.py
@@ -58,3 +58,13 @@ def create_formatter(fmt):
         return HTMLTableFormatter()
     else:
         raise RuntimeError(f"Unknown format {fmt}")
+
+
+def print_table(objects, columns, formatter):
+    """
+    Make a nicely formatted table from a list of objects and attribute names.
+    """
+    formatter.headings(columns)
+    for obj in objects:
+        rowdata = [str(getattr(obj, name)) for name in columns]
+        formatter.row(rowdata)

68a440f3a862ea463b5ed9474fd2f02043bc2670
Raymond Hunce <hunce18c@gmail.com>
2023-12-10 15:51:22 -0500

    Create source distribution

---
+++ Work/porty-app/setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+setuptools.setup(
+    name="porty",
+    version="0.0.1",
+    author="Raymond Hunce",
+    author_email="hunce18c@gmail.com",
+    description="Practical Python Code",
+    packages=setuptools.find_packages(),
+)
